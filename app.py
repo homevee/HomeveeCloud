672706439c9e9e8fbc3dd95f00217646d119c0c0
Sascha Huber <dersaschahuber@web.de>
2019-05-30 13:13:19 +0200

    app.py aufgeräumt

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 #!flask/bin/python
 import argparse
-from _thread import start_new_thread
-
 from flask import Flask
-from passlib.handlers.pbkdf2 import pbkdf2_sha512
-
-from cloud import HomeveeCloudNode
 from cloud.Database import Database
 from cloud.blueprints.AlexaAPI import AlexaAPI
 from cloud.blueprints.CloudAPI import CloudAPI
@@ -20,28 +15,11 @@ app = Flask(__name__)
 
 DEV_ENV = True
 
-def update_cert(file):
-    cert_data = open(file).read()
-    #cert_data = cert_data.replace("\n", "")
-    #cert_data = cert_data.replace("-----BEGIN CERTIFICATE-----", "")
-    #cert_data = cert_data.replace("-----END CERTIFICATE-----", "")
-
-    Database().do_query("REPLACE INTO CLOUD_CERTS (IP, CERTIFICATE) VALUES (%s, %s)",
-                        (args.ip, cert_data))
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Homevee-Cloud for Servers')
-    #parser.add_argument('--ip', required=True, type=str, help='The IP of the Server')
-    #parser.add_argument('--max_clients', default=50, type=int, help='Number of clients simultaneously')
-    #parser.add_argument('--buffer_size', default=64, type=int, help='Buffer size for client communication')
-    #parser.add_argument('--debug', default=False, type=bool, help='Is the server in debug mode?')
     parser.add_argument('--dev', default=False, type=bool, help='Is the server in dev mode?')
     parser.add_argument('--domain', required=False, default="cloud.homevee.de", type=str, help='The domain to run the cloud node on')
     args = parser.parse_args()
-
-    #hashed_access_token = pbkdf2_sha512.encrypt("+4mT6OzDRMQgZY4VX+cQGxnuSzFF4SjNFM+p/L90LdirFP6X0lIPfU8fCtfgUdoA3N+lpepN82UOgvZ/7lwV6BNYdYvNyD9duCFbNtwMpr+x+4WD5Ze0udQGvviN4W5FN4SFZ2eFh9fa2+DQdc1BBRXJKDwDpNPFPpJ1rmKsw1Y=" +
-    # "16263821935c66e615a137f9.97473803", rounds=200000)
-    #print(hashed_access_token)
 
     HOST = args.domain
 
@@ -57,21 +35,7 @@ if __name__ == '__main__':
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
 
-    #app.run(debug=True)
-
-
     if DEV_ENV:
         app.run(host='0.0.0.0', debug=True)
     else:
-        #update_cert(CERT_FILE)
-
-        #HomeveeCloudNode.DOMAIN = HOST
-        #HomeveeCloudNode.CLIENT_NUM = args.max_clients
-        #HomeveeCloudNode.IS_DEBUG = args.debug
-        #HomeveeCloudNode.IP = args.ip
-        #HomeveeCloudNode.BUFFER_SIZE = args.buffer_size
-        #HomeveeCloudNode.SERVER_SECRET = args.server_secret
-
-        #start_new_thread(HomeveeCloudNode.start, ())
-
         app.run(host=HOST, port=443, ssl_context=(FULLCHAIN_FILE, KEY_FILE))
